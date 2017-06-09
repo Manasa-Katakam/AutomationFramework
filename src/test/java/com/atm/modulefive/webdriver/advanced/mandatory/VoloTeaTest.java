80781dace7a956f17c6c6c784558fe5433be0676
Igar_Kharytonenka <Igar_Kharytonenka@EPBYMINW2898.minsk.epam.com>
2017-06-09 14:02:13 +0300

    Reviewed.

--- src/test/java/com/atm/modulefive/webdriver/advanced/mandatory/VoloTeaTest.java
+++ src/test/java/com/atm/modulefive/webdriver/advanced/mandatory/VoloTeaTest.java
@@ -35,13 +35,14 @@ public class VoloTeaTest {
 	public void LoginToVoloTea() throws InterruptedException{
 		boolean signInComplete = new VoloTeaSignIn(driver).doLogin(DataUtility.getEmail(), DataUtility.getPassword()).loginIsCorrect();
 		Assert.assertTrue(signInComplete, "User Authentication Failed, Not Logged in!");
-		System.out.println("Logged in succesfully to with the authenticated USER");
+		System.out.println("Logged in succesfully to with the authenticated USER"); // [IK] Don't put the code after assertions, because if the assertion fails, this code will not be executed.
 	}
 
 	@Test(dependsOnMethods = "LoginToVoloTea", description = "Search Flights from Prague to Venice")
 	public void EnterOriginReturnDetails() throws InterruptedException {
 		new VoloTeaFlightSearch(driver).addOriginReturnDetails();
 		System.out.println("Entered the Origin and Return Locations with specific dates");
+//		[IK] Each @Test should contain assertion. There's no assertion here.
 	}
 
 	@Test(dependsOnMethods = "EnterOriginReturnDetails", description = "Search Flights with given details")
@@ -49,14 +50,14 @@ public class VoloTeaTest {
 		new VoloTeaFlightSearch(driver).doFlightSearch(DataUtility.getChildrenCount());
 		Assert.assertTrue(new VoloTeaFlightSummary(driver).getPassengerCount().contains(PASSENGER_COUNT),
 				"Flights Search query made with incorrect passebger count");
-		System.out.println("Completed the Flight Search with specific Details");
+		System.out.println("Completed the Flight Search with specific Details"); // [IK] Don't put the code after assertions, because if the assertion fails, this code will not be executed.
 	}
 
 	@Test(dependsOnMethods = "SearchFlights", description = "Validate the Search query made previously")
 	public void FlightInformation() {
 		Assert.assertTrue(new VoloTeaFlightSummary(driver).isFlightDisplayed(),
 				"Flight Details are not displayed for the Search made!");
-		System.out.println("*****Outbound and Return Flight Details*****");
+		System.out.println("*****Outbound and Return Flight Details*****"); // [IK] Don't put the code after assertions, because if the assertion fails, this code will not be executed.
 		System.out.println(new VoloTeaFlightSummary(driver).getOriginFlightDetails());
 		System.out.println(new VoloTeaFlightSummary(driver).getReturnFlightDetails());
 		System.out.println(
