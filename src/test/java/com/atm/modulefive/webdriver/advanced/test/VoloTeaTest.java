95ed70bb5f29e315cefa93fff608a1c82d868268
Igar_Kharytonenka <Igar_Kharytonenka@EPBYMINW2898.minsk.epam.com>
2017-06-12 10:57:02 +0300

    Reviewed. Version 1.

--- src/test/java/com/atm/modulefive/webdriver/advanced/test/VoloTeaTest.java
+++ src/test/java/com/atm/modulefive/webdriver/advanced/test/VoloTeaTest.java
@@ -34,6 +34,7 @@ public class VoloTeaTest {
 	public void EnterOriginReturnDetails() throws InterruptedException {
 		new VoloTeaFlightSearch(driver).addOriginReturnDetails();
 		System.out.println("Entered the Origin and Return Locations with specific dates");
+		// [IK] Add assertion here. Each @Test should contain assertion. 
 	}
 
 	@Test(dependsOnMethods = "EnterOriginReturnDetails", description = "Search Flights with given details")
@@ -41,14 +42,14 @@ public class VoloTeaTest {
 		new VoloTeaFlightSearch(driver).doFlightSearch(DataUtility.getChildrenCount());
 		Assert.assertTrue(new VoloTeaFlightSummary(driver).getPassengerCount().contains(PASSENGER_COUNT),
 				"Flights Search query made with incorrect passebger count");
-		System.out.println("Completed the Flight Search with specific Details");
+		System.out.println("Completed the Flight Search with specific Details"); // [IK] Don't write code after assertions. It will not be executed in case the assertion fails.
 	}
 
 	@Test(dependsOnMethods = "SearchFlights", description = "Validate the Search query made previously")
 	public void FlightInformation() {
 		Assert.assertTrue(new VoloTeaFlightSummary(driver).isFlightDisplayed(),
 				"Flight Details are not displayed for the Search made!");
-		System.out.println("*****Outbound and Return Flight Details*****");
+		System.out.println("*****Outbound and Return Flight Details*****"); // [IK] Don't write code after assertions. It will not be executed in case the assertion fails.
 		System.out.println(new VoloTeaFlightSummary(driver).getOriginFlightDetails());
 		System.out.println(new VoloTeaFlightSummary(driver).getReturnFlightDetails());
 		System.out.println(
